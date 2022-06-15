2afee1ece0ff6d7574b4a4ccd6d5093d8096b737
abhishekicha <apramanik28@gmail.com>
2022-06-15 16:55:29 +0530

    regular visitor log

--- src/main/java/pages_360/VisitorsLog.java
+++ src/main/java/pages_360/VisitorsLog.java
@@ -1,11 +1,9 @@
 package pages_360;
 
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Select;
 
 public class VisitorsLog {
 	
---
+++ src/main/java/testase_360/RegularVisitorTest.java
@@ -0,0 +1,93 @@
+package testase_360;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import helper.BrowserFactory;
+import pages_360.LoginPage;
+import pages_360.ReguarVisitorLog;
+
+public class RegularVisitorTest {
+	
+
+	@Test
+	public void RegularVisitors() throws InterruptedException {
+		//launch browser
+		
+	WebDriver driver = BrowserFactory.startBrowser("chrome", "https://dev.the360.in/login");
+		
+		//created page object using page factory
+		LoginPage login_page = PageFactory.initElements(driver, LoginPage.class);
+		//call the method
+		login_page.login_to_360("surajbiswas@yopmail.com", "suraj8877");
+	
+		ReguarVisitorLog rVL = PageFactory.initElements(driver, ReguarVisitorLog.class);
+		
+		rVL.regular_visitors_log_test();
+		
+		
+	}
+	
+	@Test
+	public void RegularVisitorsSort() throws InterruptedException {
+		//launch browser
+		
+	WebDriver driver = BrowserFactory.startBrowser("chrome", "https://dev.the360.in/login");
+		
+		//created page object using page factory
+		LoginPage login_page = PageFactory.initElements(driver, LoginPage.class);
+		//call the method
+		login_page.login_to_360("surajbiswas@yopmail.com", "suraj8877");
+	
+		ReguarVisitorLog rVL = PageFactory.initElements(driver, ReguarVisitorLog.class);
+		
+		rVL.regular_visitors_log_test();
+
+		rVL.regular_visitors_sort_test();
+		
+	}
+	
+	@Test
+	public void RegularVisitorsSearch() throws InterruptedException {
+		//launch browser
+		
+	WebDriver driver = BrowserFactory.startBrowser("chrome", "https://dev.the360.in/login");
+		
+		//created page object using page factory
+		LoginPage login_page = PageFactory.initElements(driver, LoginPage.class);
+		//call the method
+		login_page.login_to_360("surajbiswas@yopmail.com", "suraj8877");
+	
+		ReguarVisitorLog rVL = PageFactory.initElements(driver, ReguarVisitorLog.class);
+		
+		rVL.searchByNameAndCategoryTest();
+		
+	}
+	
+	@Test
+	public void RegularVisitorsDetails() throws InterruptedException {
+		//launch browser
+		
+	WebDriver driver = BrowserFactory.startBrowser("chrome", "https://dev.the360.in/login");
+		
+		//created page object using page factory
+		LoginPage login_page = PageFactory.initElements(driver, LoginPage.class);
+		//call the method
+		login_page.login_to_360("surajbiswas@yopmail.com", "suraj8877");
+	
+		ReguarVisitorLog rVL = PageFactory.initElements(driver, ReguarVisitorLog.class);
+		
+		rVL.regular_visitors_details_test();
+		
+	}
+	
+	
+	 @AfterMethod(alwaysRun = true)
+	  public void teardown()
+	  {
+		BrowserFactory.quit();
+	  }
+
+}
